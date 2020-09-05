c4f37442b10a99ed19872e0afadf48a79d5853f2
Adriana Swantz <70611504+addy-cat@users.noreply.github.com>
2020-09-04 18:24:52 -0700

    Add files via upload

---
+++ add.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using org.mariuszgromada.math.mxparser;
+
+public class add : MonoBehaviour
+{
+    string userinputupperx = "Upper x bound";
+    string userinputlowerx = "Lower x bound";
+    string userinputuppery = "Upper y bound";
+    string userinputlowery = "Upper y bound";
+    public Text upperxbound;
+    public Text lowerxbound;
+    public Text upperybound;
+    public Text lowerybound;
+
+    int index = 0;
+
+    
+    public InputField input;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        upperxbound.text = userinputupperx;
+        lowerxbound.text = userinputlowerx;
+        upperybound.text = userinputuppery;
+        lowerybound.text = userinputlowery;
+
+    }
+    /*
+     * Argument x = new Argument("x");
+        Argument y = new Argument("y");
+        upperxexp = new Expression(userinputupperx, x, y);
+        */
+
+    public void getUpperX()
+    {
+        userinputupperx = input.text;
+        PlayerPrefs.SetString("UserInputUpperX", userinputupperx);
+    }
+
+    public void getLowerX()
+    {
+        userinputlowerx = input.text;
+        PlayerPrefs.SetString("UserInputLowerX", userinputlowerx);
+    }
+    public void getUpperY()
+    {
+        userinputuppery = input.text;
+        PlayerPrefs.SetString("UserInputUpperY", userinputuppery);
+
+    }
+    public void getLowerY()
+    {
+        userinputlowery = input.text;
+        PlayerPrefs.SetString("UserInputLowerY", userinputlowery);
+
+    }
+
+    public void handleInput()
+    {
+        if (index == 0)
+        {
+            getUpperX();
+        } else if (index == 1)
+        {
+            getLowerX();
+        }
+        else if (index == 2)
+        {
+            getUpperY();
+        }
+        else if (index == 3)
+        {
+            getLowerY();
+        }
+
+        index++;
+    }
+}
---
+++ graphing.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using org.mariuszgromada.math.mxparser;
+
+public class graphing : MonoBehaviour
+{
+    //for the dot
+    public GameObject point;
+    //for the unit
+    public GameObject unit;
+
+    public GameObject axis;
+
+    public Text upperx;
+    public Text lowerx;
+    public Text uppery;
+    public Text lowery;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //i is x and j is y
+        for (int i = -10; i < 100; i++)
+        {
+            for (int j = -50; j < 100; j++)
+            {
+                GameObject newUnit = Instantiate(unit);
+                newUnit.transform.position = new Vector2(i, j);
+                
+                if (j == 0)
+                {
+                    GameObject newAxis = Instantiate(axis);
+                    newAxis.transform.position = new Vector2(i, j);
+                }
+
+                if (i == 0)
+                {
+                    GameObject newAxis = Instantiate(axis, new Vector3(i, j, 0), Quaternion.AngleAxis(90, Vector3.forward));
+                    
+                }
+            }
+        }
+
+        Argument x = new Argument("x");
+        Argument y = new Argument("y");
+
+        Expression lowerxbound = new Expression(PlayerPrefs.GetString("UserInputLowerX", "-1"), x, y);
+        Expression upperxbound = new Expression(PlayerPrefs.GetString("UserInputUpperX", "-1"), x, y);
+        Expression lowerybound = new Expression(PlayerPrefs.GetString("UserInputLowerY", "-1"), x, y);
+        Expression upperybound = new Expression(PlayerPrefs.GetString("UserInputUpperY", "-1"), x, y);
+
+        //divide the RGB colors by 255
+        drawGraphInTermsOfY(lowerxbound, y, new Color(171f/255f, 235f/255f, 52f/255f, 1));
+        drawGraphInTermsOfY(upperxbound, y, new Color(235f/255f, 52f/255f, 223f/255f, 1));
+        drawGraphInTermsOfX(lowerybound, x, new Color(235f/255f, 189f/255f, 52f/255f, 1));
+        drawGraphInTermsOfX(upperybound, x, new Color(143f/255f, 52f/255f, 235f/255f, 1));
+
+        lowerx.text += PlayerPrefs.GetString("UserInputLowerX", "No lower x bound added");
+        upperx.text += PlayerPrefs.GetString("UserInputUpperX", "No upper x bound added");
+        lowery.text += PlayerPrefs.GetString("UserInputLowerY", "No lower y bound added");
+        uppery.text += PlayerPrefs.GetString("UserInputUpperY", "No upper y bound added");
+    }
+
+    //for both x bounds
+    void drawGraphInTermsOfX(Expression e, Argument y, Color colorline)
+    {
+        //make this 0 so in unity the step size is smaller than 1 or else there is weird spacing, in this
+        //way points are closer together
+        double currVar = 0;
+        
+        for (int i = 0; i < 1000; i++)
+        {
+            
+            y.setArgumentValue(currVar);
+            double currY = e.calculate();
+            //make a copy of the point object
+            GameObject newPoint = Instantiate(point);
+            SpriteRenderer renderer = newPoint.GetComponent<SpriteRenderer>();
+            renderer.color = colorline;
+
+            if (Double.IsNaN(currY))
+            {
+                currY = 0;
+            }
+            //Unity has transform->position
+            //The Vector's x and y is "i" because this is a linear function
+            newPoint.transform.position = new Vector2((float)currVar, (float)currY);
+            //step size:
+            currVar += .1;
+        }
+
+    }
+
+    //for both y bounds
+    void drawGraphInTermsOfY(Expression e, Argument x, Color colorline)
+    {
+        //make this 0 so in unity the step size is smaller than 1 or else there is weird spacing, in this
+        //way points are closer together
+        double currVar = 0;
+
+        for (int i = 0; i < 1000; i++)
+        {
+            x.setArgumentValue(currVar);
+            
+            double currX = e.calculate();
+            //make a copy of the point object
+            GameObject newPoint = Instantiate(point);
+
+            SpriteRenderer renderer = newPoint.GetComponent<SpriteRenderer>();
+            renderer.color = colorline;
+
+            if (Double.IsNaN(currX))
+            {
+                currX = 0;
+            }
+            //Unity has transform->position
+            //The Vector's x and y is "i" because this is a linear function
+            newPoint.transform.position = new Vector2((float)currX, (float)currVar);
+            //step size:
+            currVar += .1;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
