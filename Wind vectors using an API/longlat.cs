b2186ed7e4ee341bd308b082834835026ee18e8c
Adriana Swantz <70611504+addy-cat@users.noreply.github.com>
2020-12-30 21:42:41 -0800

    Update longlat.cs

--- Wind vectors using an API/longlat.cs
+++ Wind vectors using an API/longlat.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -18,14 +18,23 @@ public class longlat : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        /*
+        linkedList list = new linkedList();
+        windInfo the_info = new windInfo(4.0, 6.0);
+        windInfo the_info2 = new windInfo(2.0, 3.0);
+
+        list.add_windData(the_info);
+        list.add_windData(the_info2);
+
+        list.display_info();
+
         Text tempForecastURL = Instantiate(T1);
-        tempT1.transform.SetParent(canvas, false);
-        Text tempT2 = Instantiate(T2);
-        tempT2.transform.SetParent(canvas, false);
-        tempT1.transform.position += new Vector3(0.0f, -40.0f, 0.0f);
-        tempT2.transform.position += new Vector3(0.0f, -40.0f, 0.0f);
-        */
+
+       // tempT1.transform.SetParent(canvas, false);
+       // Text tempT2 = Instantiate(T2);
+       // tempT2.transform.SetParent(canvas, false);
+       // tempT1.transform.position += new Vector3(0.0f, -40.0f, 0.0f);
+       // tempT2.transform.position += new Vector3(0.0f, -40.0f, 0.0f);
+        
 
         StartCoroutine(GetRequest("https://api.weather.gov/points/45.7534,-121.9908", ForecastURL));
         //StartCoroutine(GetRequest("https://api.weather.gov/points/44.8869,-124.0258", tempT1, tempT2));
@@ -71,4 +80,3 @@ public class longlat : MonoBehaviour
 
 
 }
-
